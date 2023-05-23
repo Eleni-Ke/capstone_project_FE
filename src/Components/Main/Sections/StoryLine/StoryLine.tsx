af4b30831dfc8d21a83b4feb8bf05f6a48413e70
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-05-22 21:22:03 -0400

    features being built

--- src/Components/Main/Sections/StoryLine/StoryLine.tsx
+++ src/Components/Main/Sections/StoryLine/StoryLine.tsx
@@ -1,50 +1,78 @@
 import NavBar from "../../Navbar/NavBar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import ReactFlow, { Edge, Node } from "reactflow";
+// import ReactFlow, { Edge, Node } from "reactflow";
 import "reactflow/dist/base.css";
+import { useNavigate } from "react-router-dom";
+
+import { IoIosConstruct } from "react-icons/io";
 
 const StoryLine = () => {
-  const initialEdges: Edge[] = [
-    {
-      id: "e1-2",
-      source: "1",
-      target: "2",
-    },
-  ];
-  const initialNodes: Node[] = [
-    {
-      id: "1",
-      data: { label: "Input Node" },
-      type: "input",
-      position: { x: 5, y: 5 },
-    },
-    {
-      id: "2",
-      data: { label: "Output Node" },
-      type: "output",
-      position: { x: 50, y: 50 },
-    },
-    {
-      id: "3",
-      data: { label: "Default Node" },
-      type: "default",
-      position: { x: 100, y: 100 },
-    },
-  ];
+  // const initialEdges: Edge[] = [
+  //   {
+  //     id: "e1-2",
+  //     source: "1",
+  //     target: "2",
+  //   },
+  // ];
+  // const initialNodes: Node[] = [
+  //   {
+  //     id: "1",
+  //     data: { label: "Input Node" },
+  //     type: "input",
+  //     position: { x: 5, y: 5 },
+  //   },
+  //   {
+  //     id: "2",
+  //     data: { label: "Output Node" },
+  //     type: "output",
+  //     position: { x: 50, y: 50 },
+  //   },
+  //   {
+  //     id: "3",
+  //     data: { label: "Default Node" },
+  //     type: "default",
+  //     position: { x: 100, y: 100 },
+  //   },
+  // ];
+
+  const accessToken = localStorage.getItem("accessToken");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate("/");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
+    // <div className="d-flex">
+    //   <NavBar />
+
+    //   <div className="d-flex flex-column w-100">
+    //     <div className="banner-story banner">
+    //       <h2>The story line:</h2>
+    //     </div>
+    //     <div className="storyLine-main">
+    //       <ReactFlow
+    //         defaultEdges={initialEdges}
+    //         defaultNodes={initialNodes}
+    //       ></ReactFlow>
+    //     </div>
+    //   </div>
+    // </div>
     <div className="d-flex">
       <NavBar />
-
-      <div className="d-flex flex-column w-100">
-        <div className="banner-story banner">
-          <h2>The story line:</h2>
-        </div>
-        <div className="storyLine-main">
-          <ReactFlow
-            defaultEdges={initialEdges}
-            defaultNodes={initialNodes}
-          ></ReactFlow>
+      <div className="d-flex flex-column w-100 position-absolute">
+        <div className="background-table d-flex justify-content-center align-items-center">
+          <div className="p-3 contruction-message">
+            <IoIosConstruct />
+            <p>
+              This new feature is currently being built! <br /> Stay tuned to
+              see when you can use it!
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Main/Settings/Settings.tsx
+++ src/Components/Main/Settings/Settings.tsx
@@ -1,11 +1,32 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../Navbar/NavBar";
+import { IoIosConstruct } from "react-icons/io";
 
 const Settings = () => {
+  const accessToken = localStorage.getItem("accessToken");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate("/");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="d-flex">
       <NavBar />
-      <div className="d-flex flex-column w-100 position-absolute">
-        <div className="background-table"></div>
+      <div className="d-flex flex-column w-100">
+        <div className="background-table d-flex justify-content-center align-items-center">
+          <div className="p-3 contruction-message">
+            <IoIosConstruct />
+            <p>
+              This new feature is currently being built! <br /> Stay tuned to
+              see when you can use it!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
